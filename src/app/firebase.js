8c998c82f3f38c11b14431cef55fbdd739f237a0
Proshashi <proshashi11@gmail.com>
2020-12-25 00:14:33 +0545

    firebase config key change

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -4,14 +4,14 @@ import "firebase/auth";
 import "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBojvd3PWNPHs7ReWP_-stH3xz0olhAVso",
-  authDomain: "jd-gold-de5d8.firebaseapp.com",
-  databaseURL: "https://jd-gold-de5d8.firebaseio.com",
-  projectId: "jd-gold-de5d8",
-  storageBucket: "jd-gold-de5d8.appspot.com",
-  messagingSenderId: "496273232300",
-  appId: "1:496273232300:web:1843f2ebe9699ee599ea09",
-  measurementId: "G-ZD8E42NLGH",
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId,
 };
 
 firebase.initializeApp(firebaseConfig);
